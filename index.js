13c068fa22206a450596dc6263ad88d664f0ffe5
Junqi <cjqchoojunqi@gmail.com>
2021-07-06 16:32:36 +0800

    installed npm i cookie-parser and dabble with it and store token in req.cookies.token

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require("express");
 require('dotenv').config();
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 //Extracting hidden information in the .env file
 const secret = process.env.TOKEN_SECRET;
 const port = process.env.PORT;
 
+app.use(cookieParser());
 //able to use the below to generate a system generated secret for added security
 //note that the secret used cant be too simple
 //require('crypto').randomBytes(64).toString('hex')
-
 
 //function for generating a token for the user
 function generateAccessToken(userinformation){
     const {username,pwd} = userinformation;
     return jwt.sign({"username":username,"pwd":pwd},secret,{expiresIn:"1800s"})
+
 }
 
 //Authenticating the user whether they are valid using function middleware 
@@ -39,25 +41,35 @@ async function authenticateToken(req,res,next){
                 errormsg:err
             })
         }
-       console.log(decoded.iat)
     })
-    
     // will proceed on with the request if there is no error and will return and pass to the next middleware 
     next();
 }
-
 
 //route for the new user to sign up
 app.post("/api/createNewUser",(req,res)=>{
     const informationClient = {...req.body};
     const token = generateAccessToken(informationClient);
-    res.json(token);
+    //here for the parsing of the cookie
+    console.log(token);
+    res.cookie('token', token);
+    return res.json(token);
 })
 //route with sensitive information (just an example)
 app.get("/api/secretInformation",authenticateToken,(req,res)=>{
 
     //send user the response of my secret
     res.send("The secret is i love cars!")
+})
+
+
+app.get("/checkCookies",(req,res)=>{
+    const cookie = req.cookies.token;
+    if(cookie){
+        console.log(cookie)
+    }else{
+        console.log("no token is to be found anywhere u fuck tart")
+    }
 })
 
 app.listen(port,()=>{
